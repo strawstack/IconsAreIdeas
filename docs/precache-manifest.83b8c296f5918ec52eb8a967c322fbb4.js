532af4db1e27c6ad0444aa2eb9ee02e9604ffdee
Richard Hayes <rhshadowgreen@gmail.com>
2020-08-01 10:51:15 -0700

    rebuild docs

---
+++ docs/precache-manifest.83b8c296f5918ec52eb8a967c322fbb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee4e077aa26622392ff59325df8adbcb",
+    "url": "/IconsAreIdeas/index.html"
+  },
+  {
+    "revision": "d40708866fc8ff300d43",
+    "url": "/IconsAreIdeas/static/css/main.863fe687.chunk.css"
+  },
+  {
+    "revision": "82fab2728e2fbc8d80ec",
+    "url": "/IconsAreIdeas/static/js/2.cfe339b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IconsAreIdeas/static/js/2.cfe339b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d40708866fc8ff300d43",
+    "url": "/IconsAreIdeas/static/js/main.79546271.chunk.js"
+  },
+  {
+    "revision": "a62d60a7f5e11ae76acf",
+    "url": "/IconsAreIdeas/static/js/runtime-main.07a653c7.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e128dcadd33b287d78d757aadd407444.js"
+  "/IconsAreIdeas/precache-manifest.83b8c296f5918ec52eb8a967c322fbb4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IconsAreIdeas/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
