440a91523b93792d5d119f631ce2fe230bd14a3f
Richard Hayes <rhshadowgreen@gmail.com>
2020-08-01 10:40:31 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,154 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import { iconList } from './iconList.js';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { far } from '@fortawesome/pro-regular-svg-icons';
+
+library.add(far)
+
+const randomIcon = lst => {
+    const ri = Math.floor(Math.random() * lst.length);
+    return lst[ri];
+};
+
+function Button(props) {
+    return (
+        <button
+            className={styles.Button}
+            onClick={props.onClick}
+            disabled={props.disabled}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            {props.disabled ? props.disabledText : props.text}
+        </button>
+    );
+}
+function Generator(props) {
+    return (
+        <div className={styles.Generator}>
+            {props.children}
+        </div>
+    );
+}
+function ContentCenterRow(props) {
+    return (
+        <div className={styles.ContentCenterRow}>
+            {props.children}
+        </div>
+    );
+}
+function Paragraph(props) {
+    return (
+        <p className={styles.Paragraph}>
+            {props.children}
+        </p>
+    );
+}
+function Title(props) {
+    return (
+        <h1 className={styles.Title}>
+            {props.title}
+        </h1>
+    );
+}
+function ContentColumn(props) {
+    return (
+        <div className={styles.ContentColumn}>
+          {props.children}
+        </div>
+    );
+}
+function Layout(props) {
+    return (
+        <div className={styles.Layout}>
+            {props.children}
+        </div>
+    );
+}
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        // Don't call this.setState() here!
+        this.state = {
+            spinning: false,
+            icons: [
+                "question",
+                "question",
+                "question"
+            ]
+        };
+    }
+    generateBtnClicked() {
+        this.setState({spinning: true});
+        this.randomIcons();
+        const times = [250, 250, 250, 250];
+        const next = index => {
+            if (index < times.length) {
+                setTimeout(() => {
+                    this.randomIcons();
+                    next(index + 1);
+                }, times[index]);
+            } else {
+                this.setState({spinning: false});
+            }
+        };
+        next(0);
+    }
+    randomIcons() {
+        this.setState({
+            icons: [
+                randomIcon(iconList),
+                randomIcon(iconList),
+                randomIcon(iconList)
+            ]
+        });
+    }
+    render() {
+        return (
+            <Layout>
+                <ContentColumn>
+                    <Title title="Icons are Ideas" />
+                    <Paragraph>
+                        Ever have a creative block? Are you in a hackathon right now? Need an idea for your next side project? Maybe you just want to see some cool icons?
+                    </Paragraph>
+                    <Paragraph>
+                        Use the generator below to find your next idea! Your only job is to interpret the result.
+                    </Paragraph>
+                    <ContentCenterRow>
+                        <Generator>
+                            <FontAwesomeIcon
+                                icon={["far", this.state.icons[0]]}
+                                size="3x"
+                            />
+                        </Generator>
+                        <Generator>
+                            <FontAwesomeIcon
+                                icon={["far", this.state.icons[1]]}
+                                size="3x"
+                            />
+                        </Generator>
+                        <Generator>
+                            <FontAwesomeIcon
+                                icon={["far", this.state.icons[2]]}
+                                size="3x"
+                            />
+                        </Generator>
+                    </ContentCenterRow>
+                    <ContentCenterRow>
+                        <Button
+                            spinning={this.state.spinning}
+                            disabled={this.state.spinning}
+                            onClick={() => this.generateBtnClicked()}
+                            text="Generate"
+                            disabledText="Generating..."
+                        />
+                    </ContentCenterRow>
+                </ContentColumn>
+            </Layout>
+        );
+    }
 }
 
 export default App;
